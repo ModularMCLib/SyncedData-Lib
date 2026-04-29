80a891b0bfa2214afd74f275811ae827bd69939b
qiuye2024github <qiuye2024github@163.com>
2026-04-30 00:13:56 +0800

    添加部分同步类

---
+++ src/main/java/com/modularmc/synceddata/api/blockentity/BlockEntityCreationInfo.java
@@ -0,0 +1,8 @@
+package com.modularmc.synceddata.api.blockentity;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
+
+public record BlockEntityCreationInfo(BlockEntityType<?> type, BlockPos pos, BlockState state) {
+}
